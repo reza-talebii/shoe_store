2f1a97254a0611851c440356589c2744eb79e9f8
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-08 13:45:33 +0330

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={classes.navbar}>
-      <FaBars className={classes["icon-nav"]} />
+      <Link to={"/products"}>
+        <FaBars className={classes["icon-nav"]} />
+      </Link>
       <h1>انبار کفش</h1>
-      <h3 className={classes["icon-add"]}>+</h3>
+      <Link to={"/add-product"}>
+        <h3 className={classes["icon-add"]}>+</h3>
+      </Link>
     </nav>
   );
 };
