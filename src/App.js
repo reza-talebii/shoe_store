205b2e28c9a9fc38267a65882d8040157fa07e3d
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-08 09:27:38 +0330

    add pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
 //component
-import home from "./pages/home";
-import products from "./pages/products";
-import addProducts from "./pages/addProducts";
+import Home from "./pages/home";
+import Products from "./pages/products";
+import AddProducts from "./pages/addProducts";
 import "./App.css";
 
 const App = () => {
   return (
     <Routes>
-      <Route path={"/add-product"} element={<addProducts />} />
+      <Route path={"/*"} element={<Home />} />
+      <Route path={"/add-product"} element={<AddProducts />} />
+      <Route path={"/products"} element={<Products />} />
     </Routes>
   );
 };
