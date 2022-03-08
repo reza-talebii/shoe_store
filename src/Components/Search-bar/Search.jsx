872626273e4b95b88ded25938db00bddff62cf5c
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-08 10:48:58 +0330

    styled

--- src/Components/Search-bar/Search.jsx
+++ src/Components/Search-bar/Search.jsx
@@ -1,10 +1,13 @@
 import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
+import classes from "./Search.module.css";
 
 const Search = () => {
   return (
-    <form>
-      <input type="text" placeholder="سرچ ..." />
-      <FaMicrophone />
+    <form className={classes["form-search"]}>
+      <input type="text" placeholder="سرچ ..." className={classes.input} />
+      <div className={classes["voice-icon"]}>
+        <FaMicrophoneSlash />
+      </div>
     </form>
   );
 };
