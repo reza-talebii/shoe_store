edf047bcca90634a6515d1aa9d1170d2d01cba13
reza talebi <85624957+reza-talebii@users.noreply.github.com>
2022-03-09 18:36:33 +0330

    add event & state to input search

--- src/Components/Search-bar/Search.jsx
+++ src/Components/Search-bar/Search.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+//components
 import { FaMicrophone, FaMicrophoneSlash } from "react-icons/fa";
 import classes from "./Search.module.css";
 import Input from "../UI/Inputs/Input";
 
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchHandler = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <form className={classes["form-search"]}>
-      <Input type="text" placeholder="سرچ ..." />
+      <Input
+        type="text"
+        placeholder="سرچ ..."
+        value={searchValue}
+        onChange={searchHandler}
+      />
       <div className={classes["voice-icon"]}>
         <FaMicrophoneSlash />
       </div>
